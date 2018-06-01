6d01adca7cd55ca7b126d3c89fdea6b1e287625b
Maciej Bakowicz <maciej.bakowicz@nokia.com>
2018-06-01 19:16:31 +0200

    Unfix fix. Add prints

--- rosenbrock/main.py
+++ rosenbrock/main.py
@@ -1,6 +1,5 @@
 from numpy import multiply, add, array
 from random import randint
-import time
 
 start_point_x = array([0, 0])
 a = 3
@@ -9,12 +8,13 @@ S = [None, array([0, 1]), array([1, 0])]
 e = array([1, 4])
 
 gramm_schmidt_counter = 0
+steps_counter = 0
 step_success_counter = [[None], [None], [None]]
 
 
 def f(args):
     [x, y] = array(args)
-    return x*y
+    return x**5 + y*x**8 + x**9*y - y**2
 
 
 def calculate_new_coords(j):
@@ -25,10 +25,11 @@ def calculate_new_coords(j):
 
 
 def change_start_point():
-    global start_point_x
+    global start_point_x, steps_counter
+    steps_counter = 0
     value = randint(0, 256)
     diff = (value * 0.5) + 0.1
-
+    print(diff)
     if value % 2 is 0:
         start_point_x = array([start_point_x[0] + value / diff, start_point_x[1]])
     else:
@@ -36,7 +37,7 @@ def change_start_point():
 
 
 def step_forward(j):
-    global e, start_point_x
+    global e, start_point_x, steps_counter
 
     new_coords = calculate_new_coords(j)
     is_success = f(new_coords) < f(start_point_x)
@@ -51,43 +52,46 @@ def step_forward(j):
 
 
 def both_failure():
-    global step_success_counter
+    global step_success_counter, steps_counter
 
     is_failed = step_success_counter[1] == False and step_success_counter[2] == False
     return is_failed
 
 
 def gramm_schmidt():
-    global gramm_schmidt_counter, S
+    global gramm_schmidt_counter, S, steps_counter
 
+    steps_counter = 0
     gramm_schmidt_counter += 1
     S = [None, [1, 0], [0, 1]]
 
 
 def main():
-    global start_point_x, gramm_schmidt_counter
+    global steps_counter, start_point_x, gramm_schmidt_counter
 
     while True:
         for j in range(1, len(S)):
             step_forward(j)
 
         if both_failure():
-            if not step_success_counter[1]:
+            if steps_counter is 0:
+                print('Zmiana poczatkowego punktu')
                 change_start_point()
                 continue
 
             if gramm_schmidt_counter is 5:
-                print('Sukces')
+                print('Sukces w punkcie: ', start_point_x)
                 break
             else:
-                print('running gramm-schmidt')
+                print('Obrot wspolrzednych')
                 gramm_schmidt()
                 continue
 
         if gramm_schmidt_counter is not 0:
-            print(step_success_counter)
             print('Punkt startowy zostal zmieniony. Brak minimum')
             break
+
+        steps_counter += 1
 
 
 if __name__ == '__main__':
